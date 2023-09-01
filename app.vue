5b84ea4f1ddca8c4e317c9e073f83cc97b86e4d5
nipunraj <nipunraj99@gmail.com>
2023-09-01 15:39:05 +0530

    nuxt

--- app.vue
+++ app.vue
@@ -3,3 +3,8 @@
     <NuxtWelcome />
   </div>
 </template>
+<script
+  id="cookieyes"
+  type="text/javascript"
+  src="https://cdn-cookieyes.com/client_data/aff89c3e5e48bc8f53a5c41d/script.js"
+></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,3 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 });
-script: [
-  {
-    src: "https://cdn-cookieyes.com/client_data/78fa004f496bbaf1c0e09658/script.js",
-    hid: "cookieyes",
-    type: "text/javascript",
-  },
-];
