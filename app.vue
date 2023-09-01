3e2032ae64483cd87dee90bbf9d3dc9b4c90ca0a
nipunraj <nipunraj99@gmail.com>
2023-09-01 15:40:55 +0530

    nuxt

--- app.vue
+++ app.vue
@@ -2,9 +2,9 @@
   <div>
     <NuxtWelcome />
   </div>
+  <script
+    id="cookieyes"
+    type="text/javascript"
+    src="https://cdn-cookieyes.com/client_data/aff89c3e5e48bc8f53a5c41d/script.js"
+  ></script>
 </template>
-<script
-  id="cookieyes"
-  type="text/javascript"
-  src="https://cdn-cookieyes.com/client_data/aff89c3e5e48bc8f53a5c41d/script.js"
-></script>
